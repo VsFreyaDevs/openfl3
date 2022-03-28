a5e40ad4ec9f115471e80b419c3d2906c89914c1
space-nuko <space_nuko@gmail.com>
2022-03-27 23:22:49 -0700

    Adjust DisplayObject.hitTest() for inverted masks

--- src/openfl/display/OpenGLRenderer.hx
+++ src/openfl/display/OpenGLRenderer.hx
@@ -602,14 +602,7 @@ class OpenGLRenderer extends DisplayObjectRenderer
 
 		if (__stencilReference > 1)
 		{
-            // if (maskee != null && maskee.maskInverted)
-            // {
-            //     __context3D.setStencilActions(FRONT_AND_BACK, ALWAYS, INCREMENT_SATURATE, INCREMENT_SATURATE, KEEP);
-            // }
-            // else
-            // {
-                __context3D.setStencilActions(FRONT_AND_BACK, EQUAL, DECREMENT_SATURATE, DECREMENT_SATURATE, KEEP);
-            // }
+            __context3D.setStencilActions(FRONT_AND_BACK, EQUAL, DECREMENT_SATURATE, DECREMENT_SATURATE, KEEP);
 			__context3D.setStencilReferenceValue(__stencilReference, 0xFF, 0xFF);
 			__context3D.setColorMask(false, false, false, false);
 
@@ -676,12 +669,7 @@ class OpenGLRenderer extends DisplayObjectRenderer
 			__updatedStencil = true;
 		}
 
-        // if (maskee != null && maskee.maskInverted) {
-        //     __context3D.setStencilActions(FRONT_AND_BACK, ALWAYS, DECREMENT_SATURATE, DECREMENT_SATURATE, KEEP);
-        // }
-        // else {
-            __context3D.setStencilActions(FRONT_AND_BACK, EQUAL, INCREMENT_SATURATE, KEEP, KEEP);
-        // }
+        __context3D.setStencilActions(FRONT_AND_BACK, EQUAL, INCREMENT_SATURATE, KEEP, KEEP);
 		__context3D.setStencilReferenceValue(__stencilReference, 0xFF, 0xFF);
 		__context3D.setColorMask(false, false, false, false);
 
