6da5ca1f756f03c227f94d03962bd1b8e077c113
EliteMasterEric <ericmyllyoja@gmail.com>
2024-06-12 22:16:54 -0400

    Fix to class resolver to prevent a crash on some serialized data.

--- src/openfl/net/SharedObject.hx
+++ src/openfl/net/SharedObject.hx
@@ -964,6 +964,11 @@ class SharedObject extends EventDispatcher
 	{
 		if (name != null)
 		{
+			if (name == "Dynamic")
+			{
+				return null;
+			}
+
 			if (StringTools.startsWith(name, "neash."))
 			{
 				name = StringTools.replace(name, "neash.", "openfl.");
